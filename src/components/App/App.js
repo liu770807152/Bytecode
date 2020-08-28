94f6532001af9dfd2965b1584c20f71bf3062571
cathy yang <cathy.yf.yang@gmail.com>
2020-08-28 10:32:39 +1000

    new
    new

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+import Navbar from '../Navbar/Navbar';
+import Contactbasic from '../Contactbasic/Contactbasic';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <Navbar />
+      </header>
+      <Contactbasic />
+    </div>
+  )
+};
+
+export default App;
