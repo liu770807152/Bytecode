24b77c759a037cdd07b48a34ff32ee9aa75ee7bf
DerrickSong <sbz7drk@gmail.com>
2020-08-28 21:00:49 +1000

    add Derrick's files to branch

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Logo from './img/byteCRM.png';
+//import HsLogo from './logoStyle/Logo';
+import Form from './Form/Form';
+import './Form/Style/style.scss';
+
+function App() {
+  return (
+    <div>
+      <img className="byteCRMLogo" src={Logo} alt="" />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
