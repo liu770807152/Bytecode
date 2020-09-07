6e8f761af41c41cfa6e572842d8324f8cb708f37
yyr19921203 <123775172@qq.com>
2020-09-08 08:09:24 +1000

    test material ui

--- src/components/Style/Color/MatUIColor.js
+++ src/components/Style/Color/MatUIColor.js
@@ -1,15 +1,13 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
 
-
-
 function getTheme(){
     const theme = createMuiTheme({
         palette: {
           primary: {
             main: '#00bda5',
           },
-        },
+        }
       });
     return theme;
 }
